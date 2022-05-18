0fcb56c2e70db3c4e4fe8ff541eb1e501fb04698
TechnologyLand <TechnologyLand@Danny>
2022-05-17 19:01:13 -0500

    Calculadora terminada

---
+++ WebCalculadora/src/test/java/com/dxdany/WebCalculadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxdany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCalculadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
